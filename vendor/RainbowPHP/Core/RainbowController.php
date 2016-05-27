4976ff6efe148516aa5e22be78b8f72209fad423
iris-xie <443833189@qq.com>
2016-05-27 21:39:44 +0800

    Update RainbowController.php

--- vendor/RainbowPHP/Core/RainbowController.php
+++ vendor/RainbowPHP/Core/RainbowController.php
@@ -10,9 +10,11 @@ class RainbowController{
 
     public $view;
     public $database;
+    public static $instance;
     
     public function __construct()
     {
+        self::$instance =& $this;  
         if(in_array('view',$config['auto_load'])){
         //require_once BASE_PATH.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'twig'.DIRECTORY_SEPARATOR.'twig'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'Twig'.DIRECTORY_SEPARATOR.'Autoloader.php';
         $loader = new \Twig_Loader_Filesystem(BASE_PATH.'/resources/views/');
@@ -25,4 +27,10 @@ class RainbowController{
     {
      return call_user_func_array(__CLASS__,$name,$arguments);  
     }
+    
+     //这个函数对外提供了控制器的单一实例  
+    public static function &get_instance()  
+    {  
+        return self::$instance;  
+    }  
 }

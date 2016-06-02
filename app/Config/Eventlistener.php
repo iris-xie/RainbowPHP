d211d1000553f65f358ae51bcfbf6bd0c829f33a
iris-xie <443833189@qq.com>
2016-06-02 15:52:04 +0800

    //Eventlistener.php

---
+++ app/Config/Eventlistener.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Config;
+
+use RainbowPHP\Core\Route;
+class Eventlistener
+{
+
+public static $eventList = [
+              'beforeSystem' => ['App\Middle\Oauth@checkOauth'],
+              'afterSystem' => ['App\Middle\Oauth@closeOauth'],
+              'beforeRespone' => [],
+              'afterRespone' => [],
+];
+
+public static function fire ($event)
+{
+  if(array_key_exist(self::$eventList,$event)){
+    
+    foreach(self::$eventList[$event] as $val){
+        Route::runRoute($val);
+    }
+  }else {
+
+  throw EventNotFoundException($event);
+  }
+}
+
+
+
+
+
+
+
+
+
+}

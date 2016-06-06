8b708e5d892040bf0893aa33799a657966e16a57
iris-xie <443833189@qq.com>
2016-06-06 15:05:46 +0800

    修改load_class

--- vendor/RainbowPHP/Helpers/Helpers.php
+++ vendor/RainbowPHP/Helpers/Helpers.php
@@ -6,7 +6,52 @@
  * Time: 下午3:09
  */
 use RainbowPHP\Support\Str;
+ function load_class($class)  
+    {  
+        //记录加载过的类  
+        static $load_classes = array();  
+  
+        // 已经加载过，直接读取并返回  
+        if (isset($load_classes[$class]))  
+        {  
+            return $load_classes[$class];  
+        }  
+  
+        $name = FALSE;  
 
+        $yac = new Yac("rainbow_");
+        
+        if($content = $yac->get(md5($class))){
+            
+            eval($content);
+    
+        }else{
+            
+            $path = explode('\',$class);
+            
+            if(empty($path[0])) array_shift($path);
+            
+            $realpath = BASEPATH.'/vendor/'.strtoupper(join('/',$path)).'.php';
+            
+            if(!file_exists($realpth)){
+                
+                 exit('Unable to locate the specified path: '.$realpath.'.php'); 
+            }
+            
+            $content = file_get_contents($realpath);
+        
+            $yac->set(md5($class),$content);
+            
+            include $realpath;
+            
+        }
+ 
+        // 追踪记录下刚才加载的类，is_loaded()函数在下面  
+        is_loaded($class);  
+  
+        $loaded_classes[$class] = new $class();  
+        return $_classes[$class];  
+    } 
 
 if (! function_exists('env')) {
     /**
